a8e403609e96a48b387193616990912c6b9b1859
CRAZYxMUNK3Y <sharks-maniac@hotmail.com>
2020-04-11 18:14:38 +1000

    Let's start this plugin again!

---
+++ src/me/HomelessSnail/CustomDrops/CustomDropsMain.java
@@ -0,0 +1,15 @@
+package me.HomelessSnail.CustomDrops;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class CustomDropsMain extends JavaPlugin{
+
+	public void onEnable(){
+		
+	}
+	
+	public void onDisable(){
+		
+	}
+	
+}
